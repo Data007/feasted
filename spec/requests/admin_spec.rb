c7760a8e86631d29b99ff094b89c2131ab4eb7ff
lordbyron <bryan@deepwoodsbrigade.com>
2013-03-27 16:33:30 -0400

    route for /admin and controller is working correctly

---
+++ spec/requests/admin_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'Create a Wing' do
+  before do
+    visit admin_path
+  end
+
+  it 'creates wing' do
+    Wing.count.should == 0
+
+    click_link 'Room Management'
+    current_path.should == admin_wing_path
+    click_link 'Add a Wing'
+    current_path.should == new_admin_wing_path
+    fill_in 'Name', with: 'Saint Nicholas Wing'
+
+    Wing.count.should == 1
+    Wing.first.name.should == 'Sain Nicholas Wing'
+  end
+end
