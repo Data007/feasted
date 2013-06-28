63c4ba5d4cf4857173b684e1d5ac6af454f892cf
Bryan Denslow <bryan@deepwoodsbrigade.com>
2013-06-28 13:08:46 -0400

    added the days controller

--- app/controllers/admin/menus/days_controller.rb
+++ app/controllers/admin/menus/days_controller.rb
@@ -1,12 +1,31 @@
 class Admin::Menus::DaysController < Admin::MenusController
   before_filter :find_menu
+  
 
   def new
-    @day = Day.create(day: params[:id], menu_id: params[:menu_id])
-    @menu.days << @day
-    redirect_to admin_menu_day_meals_path(@menu, @day)
+    if find_day(params[:id])
+      binding.pry
+      redirect_to [:admin, @menu, @day]
+    else
+      binding.pry
+      @day = Day.create(day: params[:id], menu_id: params[:menu_id])
+      @menu.days << @day
+      redirect_to admin_menu_day_meals_path(@menu, @day)
+    end
   end
 
   def index
   end
+
+private
+  def find_day params_day
+    menu_days = []
+    menu_days = @menu.days.select {|dayofg| dayofg.day == params_day}
+    if menu_days.count >= 1
+      return false
+    else
+      return menu_days.first
+    end
+  end
+
 end
