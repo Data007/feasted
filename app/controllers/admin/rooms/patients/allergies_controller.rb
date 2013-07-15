94673c582354b945f0bfc5e1e85b972c1805b449
Bryan Denslow <bryan@deepwoodsbrigade.com>
2013-07-15 09:40:55 -0400

    can edit allergies

--- app/controllers/admin/rooms/patients/allergies_controller.rb
+++ app/controllers/admin/rooms/patients/allergies_controller.rb
@@ -1,5 +1,6 @@
 class Admin::Rooms::Patients::AllergiesController < Admin::Rooms::PatientsController
   before_filter :find_patient 
+  before_filter :find_allergy, except: [:new, :create, :index]
 
   def new
     @allergy = Allergy.new
@@ -12,5 +13,16 @@ class Admin::Rooms::Patients::AllergiesController < Admin::Rooms::PatientsContro
 
   def index
     @allergies = @patient.allergies
+  end
+
+  def update
+    @allergy.update_attributes params[:allergy]
+    redirect_to [:admin, @room, @patient, :allergies]
+  end
+
+private
+  def find_allergy
+    allergy_id = params[:allergy_id].present? ? params[:allergy_id] : params[:id]
+    @allergy = Allergy.find(allergy_id)
   end
 end
