590c25c4bfc2bebbb3e8fa03b2b43042e540f787
Bryan Denslow <bryan@deepwoodsbrigade.com>
2013-07-19 09:21:58 -0400

    change menus so that they do not rely on dates

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -2,7 +2,8 @@ class Admin::MenusController < AdminController
   before_filter :find_menu, except: [:new, :create, :index]
 
   def index
-    @menus = Menu.all
+    @menu = initialize_menu
+    redirect_to [:admin, @menu, :days]
   end
 
   def new
@@ -30,5 +31,13 @@ private
   def find_menu
     menu_id = params[:menu_id].present? ? params[:menu_id] : params[:id]
     @menu = Menu.find(menu_id)
+  end
+
+  def initialize_menu
+    if Menu.all.count >= 1
+      return Menu.all.first
+    else
+      return Menu.create
+    end
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,9 +2,6 @@ class Menu
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :name
-  field :date
-
   has_and_belongs_to_many :food
   has_many :days
 
