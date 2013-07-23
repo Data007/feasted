c2c861ed7ae1741b9d00a39b3eb6420ee7ea8872
Bryan Denslow <bryan@deepwoodsbrigade.com>
2013-07-23 13:36:52 -0400

    can deleted orders on admin side

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,18 @@
 class Admin::OrdersController < AdminController
+  before_filter :find_order, except: [:index] 
   def index
     @orders = Order.all.select {|order| order.completed?}
     @orders = @orders.select {|order| order.created_at.today?}
+  end
+
+  def destroy
+    @order.destroy
+    redirect_to [:admin, :orders] 
+  end
+
+private
+  def find_order
+    order_id = params[:order_id].present? ? params[:order_id] : params[:id]
+    @order = Order.find(order_id)
   end
 end
